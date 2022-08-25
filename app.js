9b80107f22005dd6344d33b3c465e104239edde9
Yasas <ymeka2000@gmail.com>
2022-08-25 16:45:06 +0530

    yasas

---
+++ app.js
@@ -0,0 +1,3 @@
+var logger=require('./logger.js');
+logger('Message');
+//console.log(logger.endPoint);
---
+++ logger.js
@@ -0,0 +1,8 @@
+var x=;
+var url='http://mylogger.io/log';
+function log(message){
+    //sent an http req
+    console.log(message);
+}
+module.exports=log;
+//module.exports.endPoint=url;
